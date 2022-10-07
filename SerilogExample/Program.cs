7a23218ca7b0518737e5a81ce2b4f2871097e310
Mike Wood <Mike.Wood@brokerdirect.co.uk>
2022-10-07 14:39:41 +0100

    added aerilog

---
+++ SerilogExample/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Serilog.Events;
+using Serilog;
+
+Console.WriteLine("Hello, World!");
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Debug(LogEventLevel.Verbose)
+    .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information) // restricted... is Optional
+    .CreateLogger();
+
+Log.Error("some error");
+
+
+Log.Information("This is for a user");
+Log.Debug("Some other issue for debug");
+
+Console.ReadLine();
